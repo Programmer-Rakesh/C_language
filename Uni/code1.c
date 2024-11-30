2d7a5253bc73146edcf577f44b0773f14fae9c55
Programmer-Rakesh <rakeshaps8866@gmail.com>
2024-11-30 20:17:56 +0530

    New Files

--- Uni/code1.c
+++ Uni/code1.c
@@ -1,2 +1,14 @@
 #include <stdio.h>
 
+
+int Add(int i, int j, int k){
+    return i + j + k;
+}
+int main(){
+    int arr[4]= {2,3,4,5};
+    int d = Add( arr[1], arr[2], arr[3]);
+
+    printf("%d", d);
+
+    return 0;
+}
