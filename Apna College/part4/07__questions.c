e828999c6092fc9a52f77f0b50d444421b640529
Programmer-Rakesh <rakeshaps8866@gmail.com>
2024-12-06 20:13:56 +0530

    Done for today

--- Apna College/part4/07__questions.c
+++ Apna College/part4/07__questions.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
 int main(){
-    // Print the numbers forom 0 to n, if n is given by user
-    // n = 4
+    // Print the numbers from 0 to n, if n is given by user
 
     int n;
     printf("Enter the number : ");
@@ -13,6 +12,20 @@ int main(){
         printf("%d \n", i);
         i++;
     }
+    
+
+    // WAP to print sum of n terms
+    int a;
+    printf("Enter your number : ");
+    scanf("%d", &a);
+
+    int sum = 0;
+    for (int j=1; j<=a; j++)
+    {
+        sum = sum + j;
+    }
+
+    printf("Sum is : %d \n ", sum);
 
     return 0;
 }
---
+++ Apna College/part4/08__do-WhileLoop.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    // WAP to print number 1 to 5 using do while loop
+
+    int i = 1;
+    do{
+        printf("%d \n", i);
+        i++;
+    }while(i<=5);
+
+
+    
+
+    return 0;
+}
